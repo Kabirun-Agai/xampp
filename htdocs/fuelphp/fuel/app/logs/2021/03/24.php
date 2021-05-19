97bc81e0a0f117be1d91557e038e3dd0cefba988
Kabirun-Agai <ki1209ki1999@gmail.com>
2021-05-19 19:39:30 +0900

    Folder sharing

---
+++ htdocs/fuelphp/fuel/app/logs/2021/03/24.php
@@ -0,0 +1,6 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2021-03-24 03:42:52 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-03-24 07:55:36 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-03-24 08:16:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-03-24 15:08:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
---
+++ htdocs/fuelphp/fuel/app/logs/2021/03/30.php
@@ -0,0 +1,8 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2021-03-30 17:36:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-03-30 17:40:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-03-30 17:48:28 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-03-30 18:13:45 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-03-30 18:14:28 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2021-03-30 18:14:35 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
